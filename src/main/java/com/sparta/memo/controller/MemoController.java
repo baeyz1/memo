42a7797d5d6cabf8e28472b1c29c6fb608084c87
qoduswn9810@gmail.com <Robbie@email.com>
2025-07-24 21:30:28 +0900

    5 Memo 생성 API 구현 및 Memo 생성 로직 작성

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,36 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/menos")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping
+    public MemoResponseDto CreateMemo(@RequestBody MemoRequestDto) {
+
+        //식별자가 1씩 증가하도록 만듦
+        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+
+        //요청받은 데이터로 Memo 객체 생성
+        Memo memo = new Memo(memoId, dto.getTitle(), dto.getContents());
+
+        //Inmemory DB에 Memo 메모
+        memoList.put(memoId, memo);
+
+        return new MemoResponseDto(memo);
+
+    }
+}
--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -1,5 +1,6 @@
 package com.sparta.memo.dto;
 
+import com.sparta.memo.entity.Memo;
 import lombok.Getter;
 
 @Getter
@@ -8,4 +9,10 @@ public class MemoResponseDto {
     private Long id;
     private String title;
     private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.title = memo.getTitle();
+        this.contents = memo.getContents();
+    }
 }
