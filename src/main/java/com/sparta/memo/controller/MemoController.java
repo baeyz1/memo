d4e77d266976eb5db0f55d717ab5cb5f3f39a64d
yeonju <qoduswn9810@gmail.com>
2025-07-25 22:45:16 +0900

    memo project 6 메모 단 건 제목 수정 기능

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -84,6 +84,25 @@ public class MemoController {
         return new ResponseEntity<>(MemoResponseDto(memo), HttpStatus.OK);
     }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto dto
+    ) {
+        Memo memo = memoList.get(id);
+        //NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        //필수값 방지
+        if (dto.getTitle() == null || dto.getContents() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        memo.updateTitle(dto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo),  HttpStatus.OK);
+    }
+
     @DeleteMapping("/{id}")
     public void deleteMemo(@PathVariable Long id) {
 
